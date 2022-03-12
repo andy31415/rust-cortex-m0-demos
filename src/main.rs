14a9ffb4fb3884937c88fd0ada299fd81cbf6993
Andrei Litvin <andy314@gmail.com>
2022-03-12 12:31:26 -0500

    AHT10 demo, with local path

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use panic_rtt_target as _;
 use cortex_m_rt::entry;
 use stm32f0xx_hal as hal;
 
-use crate::hal::{delay::Delay, pac, prelude::*};
+use crate::hal::{delay::Delay, i2c, pac, prelude::*};
 
+use aht10::AHT10;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
@@ -25,15 +26,42 @@ fn main() -> ! {
         .freeze(&mut pac_peripherals.FLASH);
 
     let mut delay = Delay::new(cortex_peripherals.SYST, &rcc);
+    let gpio = pac_peripherals.GPIOB.split(&mut rcc);
 
-    let gpio = pac_peripherals.GPIOC.split(&mut rcc);
-    let mut led = cortex_m::interrupt::free(move |cs| gpio.pc13.into_push_pull_output(cs));
+    let i2c = i2c::I2c::i2c1(
+        pac_peripherals.I2C1,
+        cortex_m::interrupt::free(move |cs| {
+            (
+                gpio.pb6.into_alternate_af1(cs),
+                gpio.pb7.into_alternate_af1(cs),
+            )
+        }),
+        100.khz(),
+        &mut rcc,
+    );
 
-    rprintln!("LED initialized. Will blink now.");
+    let maybe_aht10 = AHT10::new(i2c, delay.clone());
+    if let Err(err) = maybe_aht10 {
+        rprintln!("I2C error: {:?}", err);
+        loop {}
+    }
+    let mut aht10 = maybe_aht10.unwrap();
 
     loop {
-        // your code goes here
-        delay.delay_ms(300_u16);
-        led.toggle().ok();
+        if let Ok((humidity, temp)) = aht10.read() {
+            rprintln!(
+                "Temperature {}C humidity {}%.",
+                temp.celsius(),
+                humidity.rh()
+            );
+            rprintln!(
+                "RAW Temperature {} humidity {}.",
+                temp.raw(),
+                humidity.raw()
+            );
+        } else {
+            rprintln!("Read error.");
+        }
+        delay.delay_ms(1000_u16);
     }
 }
