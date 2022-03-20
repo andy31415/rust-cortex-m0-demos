7d812124d36304994c6fd4c7a3fa5a55b63a5243
Andrei Litvin <andy314@gmail.com>
2022-03-20 18:40:57 -0400

    Split constants to a library and make it work without memory limits ... neat!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(alloc_error_handler)]
 #![feature(generic_const_exprs)]
-#![cfg_attr(not(test), no_std)]
+#![no_std]
 #![no_main]
 
 use embedded_hal::spi::{Mode, Phase, Polarity};
@@ -21,78 +21,23 @@ use crate::hal::{pac, prelude::*};
 use rtt_target::{rprintln, rtt_init_print};
 use smart_leds::RGB8;
 
-macro_rules! bit_3_u32 {
-    ($x:expr) => { if $x { 0b110 } else { 0b100} as u32 }
-}
+use spi_constants::ws2812_constants;
 
-macro_rules! nibble_12_u32 {
-    ($x:expr) => {
-        (bit_3_u32!(($x & 0b1000) != 0) << 9)
-            | (bit_3_u32!(($x & 0b0100) != 0) << 6)
-            | (bit_3_u32!(($x & 0b0010) != 0) << 3)
-            | (bit_3_u32!(($x & 0b0001) != 0) << 0)
-    };
-}
+ws2812_constants!(WRITE_3_BYTE_CONSTANTS);
 
-const NIBBLE_MAP: [u32; 16] = [
-    nibble_12_u32!(0),
-    nibble_12_u32!(1),
-    nibble_12_u32!(2),
-    nibble_12_u32!(3),
-    nibble_12_u32!(4),
-    nibble_12_u32!(5),
-    nibble_12_u32!(6),
-    nibble_12_u32!(7),
-    nibble_12_u32!(8),
-    nibble_12_u32!(9),
-    nibble_12_u32!(10),
-    nibble_12_u32!(11),
-    nibble_12_u32!(12),
-    nibble_12_u32!(13),
-    nibble_12_u32!(14),
-    nibble_12_u32!(15),
-];
-
-type LedType = RGB8;
-
-fn read_into_bytes(val: u8, dest: &mut [u8]) {
-    let val = val as usize;
-    let data = NIBBLE_MAP[val & 0x0F] | (NIBBLE_MAP[(val >> 4) & 0x0F] << 12);
-
-    dest[0] = ((data >> 16) & 0xFF) as u8;
-    dest[1] = ((data >> 8) & 0xFF) as u8;
-    dest[2] = (data & 0xFF) as u8;
-}
-
-fn get_bytes(val: RGB8) -> [u8; 9] {
-    let mut values = [0u8; 9];
-
-    read_into_bytes(val.g, &mut values[0..3]);
-    read_into_bytes(val.r, &mut values[3..6]);
-    read_into_bytes(val.b, &mut values[6..9]);
-
-    values
-}
-
-pub struct SpiWrapper<SPI, const MAX_LEDS: usize>
+pub struct SpiWrapper<SPI>
 where
     SPI: embedded_hal::blocking::spi::Write<u8> + embedded_hal::blocking::spi::Transfer<u8>,
-    [u8; 9 * MAX_LEDS]: Sized,
 {
     spi: SPI,
-    buffer: [u8; 9 * MAX_LEDS],
 }
 
-impl<SPI, const MAX_LEDS: usize> SpiWrapper<SPI, MAX_LEDS>
+impl<SPI> SpiWrapper<SPI>
 where
     SPI: embedded_hal::blocking::spi::Write<u8> + embedded_hal::blocking::spi::Transfer<u8>,
-    [u8; 9 * MAX_LEDS]: Sized,
 {
     fn new(spi: SPI) -> Self {
-        SpiWrapper {
-            spi,
-            buffer: [0; 9 * MAX_LEDS],
-        }
+        SpiWrapper { spi }
     }
 
     fn flush(&mut self) -> anyhow::Result<()> {
@@ -104,13 +49,12 @@ where
     }
 }
 
-impl<SPI, const MAX_LEDS: usize> SmartLedsWrite for SpiWrapper<SPI, MAX_LEDS>
+impl<SPI> SmartLedsWrite for SpiWrapper<SPI>
 where
     SPI: embedded_hal::blocking::spi::Write<u8> + embedded_hal::blocking::spi::Transfer<u8>,
-    [u8; 9 * MAX_LEDS]: Sized,
 {
     type Error = anyhow::Error;
-    type Color = LedType;
+    type Color = RGB8;
 
     /// Write all the items of an iterator to a ws2812 strip
     fn write<T, I>(&mut self, iterator: T) -> Result<(), anyhow::Error>
@@ -118,23 +62,20 @@ where
         T: Iterator<Item = I>,
         I: Into<Self::Color>,
     {
-        let mut pos = 0;
+        let mut buffer = [0u8; 12];
         for item in iterator {
             let item = item.into();
-            let data = get_bytes(item);
-            for b in data {
-                self.buffer[pos] = b;
-                pos += 1;
-            }
-        }
 
-        self.spi
-            .write(&self.buffer[0..pos])
-            .map_err(|_e| anyhow::anyhow!("Write error"))?;
+            buffer[0..4].copy_from_slice(&WRITE_3_BYTE_CONSTANTS[item.g as usize]);
+            buffer[4..8].copy_from_slice(&WRITE_3_BYTE_CONSTANTS[item.r as usize]);
+            buffer[8..12].copy_from_slice(&WRITE_3_BYTE_CONSTANTS[item.b as usize]);
+
+            self.spi
+                .write(&buffer)
+                .map_err(|_e| anyhow::anyhow!("Write error"))?;
+        }
         self.flush()?;
 
-        // Now, resolve the offset we introduced at the beginning
-        // TODO: is a read needed? why?
         Ok(())
     }
 }
@@ -167,6 +108,9 @@ fn main() -> ! {
         .sysclk(48.mhz())
         .freeze(&mut pac_peripherals.FLASH);
 
+    rprintln!("SClock: {} Hz", rcc.clocks.sysclk().0);
+    rprintln!("PClock: {} Hz", rcc.clocks.pclk().0);
+
     let mut delay = Delay::new(cortex_peripherals.SYST, &rcc);
 
     let gpioa = pac_peripherals.GPIOA.split(&mut rcc);
@@ -186,17 +130,15 @@ fn main() -> ! {
             polarity: Polarity::IdleLow,
             phase: Phase::CaptureOnFirstTransition,
         },
-        // 38000.khz(),
-        3000.khz(),
-        //2500.khz(),
+        2400.khz(),
         &mut rcc,
     );
 
     let spi = spi.into_8bit_width();
 
     const NUM_LEDS: usize = 10;
-    let mut spi: SpiWrapper<_, NUM_LEDS> = SpiWrapper::new(spi);
-    let mut data = [LedType::default(); NUM_LEDS];
+    let mut spi = SpiWrapper::new(spi);
+    let mut data = [RGB8::default(); NUM_LEDS];
 
     rprintln!("Ready to run.");
     loop {
@@ -205,6 +147,7 @@ fn main() -> ! {
             for (i, data) in data.iter_mut().enumerate() {
                 *data = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
             }
+
             spi.write(brightness(data.iter().cloned(), 32)).unwrap();
             delay.delay_ms(5u8);
         }
