2b6ed85844e5178be14f2934863b4183c162aeef
Andrei Litvin <andy314@gmail.com>
2022-05-07 08:50:48 -0400

    make the motor spin in a very basic way

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use cortex_m::Peripherals;
+use embedded_hal::digital::v2::OutputPin;
 use panic_rtt_target as _;
 
 use cortex_m_rt::entry;
@@ -10,6 +11,40 @@ use stm32f0xx_hal as hal;
 use crate::hal::{delay::Delay, pac, prelude::*};
 
 use rtt_target::{rprintln, rtt_init_print};
+
+enum A4988State {
+    On,
+    Off,
+}
+
+struct A4988<ControlPin: OutputPin> {
+    control: ControlPin,
+    state: A4988State,
+}
+
+impl<ControlPin: OutputPin> A4988<ControlPin> {
+    fn new(control: ControlPin) -> Self {
+        Self {
+            control,
+            state: A4988State::Off,
+        }
+    }
+
+    fn step(&mut self) -> Result<(), ControlPin::Error> {
+        match self.state {
+            A4988State::On => {
+                self.control.set_low()?;
+                self.state = A4988State::Off;
+            }
+            A4988State::Off => {
+                self.control.set_high()?;
+                self.state = A4988State::On;
+            }
+        }
+
+        Ok(())
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -26,14 +61,19 @@ fn main() -> ! {
 
     let mut delay = Delay::new(cortex_peripherals.SYST, &rcc);
 
-    let gpio = pac_peripherals.GPIOC.split(&mut rcc);
-    let mut led = cortex_m::interrupt::free(move |cs| gpio.pc13.into_push_pull_output(cs));
+    let gpioc = pac_peripherals.GPIOC.split(&mut rcc);
+    let mut led = cortex_m::interrupt::free(move |cs| gpioc.pc13.into_push_pull_output(cs));
+
+    let gpiob = pac_peripherals.GPIOB.split(&mut rcc);
+    let motor = cortex_m::interrupt::free(move |cs| gpiob.pb3.into_push_pull_output(cs));
+
+    let mut motor = A4988::new(motor);
 
     rprintln!("LED initialized. Will blink now.");
 
     loop {
-        // your code goes here
-        delay.delay_ms(300_u16);
+        delay.delay_ms(20_u16);
         led.toggle().ok();
+        motor.step().ok();
     }
 }
