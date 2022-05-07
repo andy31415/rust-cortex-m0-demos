0eb42b78b60f4801d65b48fd23dea8e104103aea
Andrei Litvin <andy314@gmail.com>
2022-05-07 10:08:14 -0400

    Interrupts work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+use core::ops::DerefMut;
+
+use crate::hal::time::Hertz;
+use cortex_m::interrupt::free;
+use cortex_m::interrupt::Mutex;
 use cortex_m::Peripherals;
+use hal::timers::Timer;
 use panic_rtt_target as _;
 
 use cortex_m_rt::entry;
+use hal::stm32::interrupt;
+use hal::stm32::Interrupt;
+use stm32f0::stm32f0x0::TIM3;
 use stm32f0xx_hal as hal;
 
 use crate::hal::{delay::Delay, pac, prelude::*};
@@ -100,6 +110,8 @@ mod a4988 {
     }
 }
 
+static MOTOR_TIMER: Mutex<RefCell<Option<Timer<TIM3>>>> = Mutex::new(RefCell::new(None));
+
 // Move:
 //   turn(degrees).in(ms)
 //
@@ -132,6 +144,19 @@ fn main() -> ! {
 
     rprintln!("Ready to start");
 
+    //let mut t = Timer::tim1(pac_peripherals.TIM3, KiloHertz(1), &mut rcc);
+    let mut t3 = Timer::tim3(pac_peripherals.TIM3, Hertz(1), &mut rcc);
+    t3.listen(hal::timers::Event::TimeOut);
+
+    free(|cs| {
+        MOTOR_TIMER.borrow(cs).borrow_mut().replace(t3);
+    });
+
+    hal::pac::NVIC::unpend(Interrupt::TIM3);
+    unsafe {
+        hal::pac::NVIC::unmask(Interrupt::TIM3);
+    }
+
     // Full turn, 5 ticks per turn. At 1KHz this is 1rot/sec
     motor.start_turning(200, 5).ok();
 
@@ -151,4 +176,21 @@ fn main() -> ! {
             }
         }
     }
+}
+
+#[interrupt]
+fn TIM3() {
+    static mut CNT: u32 = 0;
+    rprintln!("Interrupt {}", CNT);
+    *CNT += 1u32;
+
+    hal::pac::NVIC::unpend(stm32f0::stm32f0x0::Interrupt::TIM3);
+
+    free(|cs| {
+        if let Some(ref mut t3) = MOTOR_TIMER.borrow(cs).borrow_mut().deref_mut() {
+            t3.wait().ok(); // clear the wrapped bit
+        }
+    })
+
+    // TODO: pending clear
 }
